d2f72287fe217814e76e1b778988680a8ade82ff
Abdoulaye Siby <layediallo160@gmail.com>
2023-12-30 17:00:34 -0800

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int digit = 0;
+
+    while (digit <= 9)
+    {
+        putchar(digit + '0');
+        digit++;
+    }
+
+    putchar('\n');  /* New line after printing the numbers */
+    return 0;
+}
+
