2ff38cd744f655c43d7f775a3b97b482b0ebe322
Abdoulaye Siby <layediallo160@gmail.com>
2023-12-31 12:24:13 -0800

    more functions nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,13 +9,13 @@ void print_diagonal(int n)
     if (n <= 0)
     {
         _putchar('\n');
-    }else
+    } else
     {
 	    int i, j;
     
             for (i = 0; i < n; i++)
 	    {
-		    for (j = 0; j < i; j++)
+		    for (j = 0; j < n; j++)
         {
 		if (j == i)
 			_putchar('\\ ');
