2801627e536e163a5aa29d66420e4ac5596ac929
Abdoulaye Siby <layediallo160@gmail.com>
2023-12-31 09:28:52 -0800

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle followed by a new line
+ * @size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+    if (size <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    int row, space, hash;
+
+    for (row = 0; row < size; row++)
+    {
+        for (space = 0; space < size - row - 1; space++)
+        {
+            _putchar(' ');
+        }
+
+        for (hash = 0; hash < row + 1; hash++)
+        {
+            _putchar('#');
+        }
+
+        _putchar('\n');
+    }
+}
+
