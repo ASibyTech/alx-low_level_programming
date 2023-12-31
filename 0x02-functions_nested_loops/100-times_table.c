876b5f2fca4f4b8ce23c0ce71fbc653005070026
Abdoulaye Siby <layediallo160@gmail.com>
2023-12-30 19:08:31 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The times table to print.
+ */
+void print_times_table(int n)
+{
+    if (n < 0 || n > 15)
+        return;
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            int result = i * j;
+
+            if (j == 0)
+                printf("%d", result);
+            else
+                printf(",%3d", result);
+        }
+        printf("\n");
+    }
+}
+
