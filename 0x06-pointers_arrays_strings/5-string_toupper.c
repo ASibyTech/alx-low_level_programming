6d4207736f4696766ffc90a79eab601cf0399060
Abdoulaye Siby <layediallo160@gmail.com>
2024-01-05 08:32:03 -0800

    pointer

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,16 +6,19 @@
  *
  * Return: Pointer to the modified string.
  */
-char *string_toupper(char *str) {
-    int i = 0;
 
-    while (str[i] != '\0') {
-        if (str[i] >= 'a' && str[i] <= 'z') {
-            str[i] = str[i] - ('a' - 'A');
-        }
-        i++;
-    }
-
-    return str;
+char *string_toupper(char *str)
+{
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+		i++;
+	}
+	return str;
 }
 
