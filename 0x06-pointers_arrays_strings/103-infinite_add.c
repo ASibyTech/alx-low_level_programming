401b8412a0ece1fd7b781e3b334770c1487fc5c3
Abdoulaye Siby <layediallo160@gmail.com>
2024-01-05 09:35:15 -0800

    pointer

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,37 +7,41 @@
  * @n2: The second number as a string.
  * @r: The buffer to store the result.
  * @size_r: The size of the buffer.
- *
- * Return: A pointer to the result, or 0 if the result can't be stored.
+ * Return: A pointer to the result
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r) {
-    int i, j, k, carry, sum;
 
-    i = j = k = carry = sum = 0;
-
-    while (n1[i] != '\0' || n2[j] != '\0' || carry) {
-        sum = carry;
-
-        if (n1[i] != '\0') {
-            sum += (n1[i] - '0');
-            i++;
-        }
-
-        if (n2[j] != '\0') {
-            sum += (n2[j] - '0');
-            j++;
-        }
-
-        if (k < size_r - 1) {
-            r[k] = (sum % 10) + '0';
-            k++;
-        } else {
-            return 0;  /* Result can't be stored in the buffer */
-        }
-
-        carry = sum / 10;
-    }
-
-    r[k] = '\0';
-    return r;
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j, k, carry, sum;
+	
+	i = j = k = carry = sum = 0;
+	
+	while (n1[i] != '\0' || n2[j] != '\0' || carry)
+	{
+		sum = carry;
+		
+		if (n1[i] != '\0')
+		{
+			sum += (n1[i] - '0');
+			i++;
+		}
+		if (n2[j] != '\0')
+		{
+			sum += (n2[j] - '0');
+			j++;
+		}
+		
+		if (k < size_r - 1)
+		{
+			r[k] = (sum % 10) + '0';
+			k++;
+		} else
+		{
+			return 0;  /* Result can't be stored in the buffer */
+		}
+		
+		carry = sum / 10;
+	}
+	r[k] = '\0';
+	return r;
 }
