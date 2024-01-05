b6199ceb1801c3911448f92770c551803ced8379
Abdoulaye Siby <layediallo160@gmail.com>
2024-01-05 08:10:56 -0800

    pointer

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,15 +4,16 @@
  * _strcmp - Compares two strings.
  * @s1: The first string to be compared.
  * @s2: The second string to be compared.
- *
- * Return: Difference between ASCII values of the first non-matching characters.
+ * Return: Difference between ASCII
  */
-int _strcmp(char *s1, char *s2) {
-    while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2) {
-        s1++;
-        s2++;
-    }
 
-    return (*s1 - *s2);
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
 
