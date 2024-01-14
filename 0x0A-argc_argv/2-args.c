df7a9441042619be5d4b746daffd4d6cd0f76e8e
Abdoulaye Siby <layediallo160@gmail.com>
2024-01-13 18:48:47 -0800

    argc, argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments passed to the program
+ * @argv: An array containing the arguments passed to the program
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+    int i;
+
+    for (i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+
+    return 0;
+}
+
