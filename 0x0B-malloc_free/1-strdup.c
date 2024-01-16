00d8080cd7d5764fb842387cdbb6238aa7bc7b77
Abdoulaye Siby <layediallo160@gmail.com>
2024-01-16 14:02:26 -0800

    molloc, free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - Duplicates a string.
+ * @str: The string to duplicate.
+ *
+ * Return: A pointer to the duplicated string.
+ *         NULL if str is NULL or if memory allocation fails.
+ */
+char *_strdup(char *str)
+{
+    char *dup_str;
+    unsigned int length = 0, i;
+
+    if (str == NULL)
+        return (NULL);
+
+    /* Calculate the length of the string */
+    while (str[length])
+        length++;
+
+    /* Allocate memory for the duplicated string */
+    dup_str = malloc((length + 1) * sizeof(char));
+    if (dup_str == NULL)
+        return (NULL);
+
+    /* Copy the string to the newly allocated memory */
+    for (i = 0; i <= length; i++)
+        dup_str[i] = str[i];
+
+    return (dup_str);
+}
