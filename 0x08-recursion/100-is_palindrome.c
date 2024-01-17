cdef121306b45b3ec9f08992aab6898fe5cd83ea
Abdoulaye Siby <layediallo160@gmail.com>
2024-01-17 12:05:20 -0800

    is palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,10 +11,10 @@ int is_palindrome(char *s)
 {
 	int length = strlen(s);
 
-	if (length <= 1)  /* An empty string or a string with a single character is a palindrome */
-		return 1;
+	if (length <= 1)
+		return (1);
 
-	return check_palindrome(s, 0, length - 1);  /* Call a helper function to check for palindrome */
+	return (check_palindrome(s, 0, length - 1));
 }
 
 /**
@@ -27,11 +27,11 @@ int is_palindrome(char *s)
  */
 int check_palindrome(char *s, int start, int end)
 {
-	if (start >= end)  /* Base case: if start is greater than or equal to end, the string is a palindrome */
-		return 1;
+	if (start >= end)
+		return (1);
 
-	if (s[start] != s[end])  /* If characters at start and end are not equal, the string is not a palindrome */
-		return 0;
+	if (s[start] != s[end])
+		return (0);
 
-	return check_palindrome(s, start + 1, end - 1);  /* Recursive call to check the next pair of characters */
+	return (check_palindrome(s, start + 1, end - 1));
 }
