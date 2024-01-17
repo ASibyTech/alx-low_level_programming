23c786f5f77b4eed7499599476641dda3351ce21
Abdoulaye Siby <layediallo160@gmail.com>
2024-01-17 12:09:19 -0800

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,15 +10,15 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')  /* If both strings are at the end, they are identical */
-		return 1;
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
-	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')  /* If s2 has '*' and s1 is at the end, check the next character in s2 */
-		return 0;
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
 
-	if (*s2 == '*' || *s1 == *s2)  /* If characters match or s2 has '*', proceed with the next characters in both strings */
-		return wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
+	if (*s2 == '*' || *s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
-	return 0;  /* If characters do not match and s2 does not have '*', strings are not identical */
+	return (0);
 }
 
