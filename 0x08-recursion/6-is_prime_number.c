3f3628a50217eb642a52e9016428e57135883670
Abdoulaye Siby <layediallo160@gmail.com>
2024-01-17 12:00:59 -0800

    is prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,10 +8,10 @@
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)  /* If the number is less than or equal to 1, it's not prime */
-		return 0;
+	if (n <= 1)
+		return (0);
 
-	return check_prime(n, 2);  /* Call a helper function to check for primality, starting with divisor 2 */
+	return (check_prime(n, 2));
 }
 
 /**
@@ -23,11 +23,11 @@ int is_prime_number(int n)
  */
 int check_prime(int n, int divisor)
 {
-	if (divisor > n / 2)  /* Base case: if the divisor is greater than half of n, n is prime */
-		return 1;
+	if (divisor > n / 2)
+		return (1);
 
-	if (n % divisor == 0)  /* If n is divisible by the current divisor, it's not prime */
-		return 0;
+	if (n % divisor == 0)
+		return (0);
 
-	return check_prime(n, divisor + 1);  /* Recursive call to check with the next divisor */
+	return (check_prime(n, divisor + 1));
 }
