bf8f9aa88e53e768245d7bd534ae16a1d64a84a2
Abdoulaye Siby <layediallo160@gmail.com>
2024-01-18 14:43:17 -0800

    more malloc free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,15 +15,11 @@ int *array_range(int min, int max)
 
 	if (min > max )
 		return (NULL);
-
 	size = max - min + 1;
 	arr = malloc(sizeof(int) * size);
-
 	if (arr == NULL)
 		return (NULL);
-
 	for (i = 0; i < size; i++)
 		arr[i] = min++;
-
 	return (arr);
 }
